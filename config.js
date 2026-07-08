2cc3374da99085814d5f79055b65253322d4ee0a
sebastianoscardaniele-star <sebastianoscardaniele@gmail.com>
2026-07-08 15:42:40 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// CONFIGURACION SUPABASE
-// Reemplazar con tus datos reales de Supabase > Project Settings > API
-window.SUPABASE_URL = "PEGAR_URL_DE_SUPABASE";
-window.SUPABASE_ANON_KEY = "PEGAR_ANON_KEY_DE_SUPABASE";
